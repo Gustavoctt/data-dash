32909f1a0ab711fe84290b43f9d69d1a364730e5
Gustavoctt <gustavocarrertartare@gmail.com>
2023-04-03 14:09:38 -0300

    feat: create sidebar component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,53 @@
+import * as S from "./styles";
+import { Engine, Users, House, Gear } from "@phosphor-icons/react";
+import { Link, useLocation } from "react-router-dom";
+
+export function Sidebar() {
+  const location = useLocation();
+
+  const buttons = [
+    {
+      id: "assets",
+      label: "Assets",
+      path: "/",
+      icon: <Engine size={24} />,
+    },
+    {
+      id: "company",
+      path: "/asset/1",
+      label: "Empresas",
+      icon: <House size={24} />,
+    },
+    {
+      id: "units",
+      path: "/units",
+      label: "Unidades",
+      icon: <Gear size={24} />,
+    },
+    {
+      id: "users",
+      path: "/users",
+      label: "Usuários",
+      icon: <Users size={24} />,
+    },
+  ];
+
+  const isSelected = (button: typeof buttons[number]) => {
+    if (location.pathname === "/") return button.path === location.pathname;
+
+    return location.pathname.split("/")[1] === button.path?.substring(1);
+  };
+
+  return (
+    <S.Sidebar>
+      {buttons.map((button) => (
+        <Link to={button.path} key={button.id}>
+          <S.SidebarContent isSelected={isSelected(button)}>
+            <>{button.icon}</>
+            <p>{button.label}</p>
+          </S.SidebarContent>
+        </Link>
+      ))}
+    </S.Sidebar>
+  );
+}
---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,40 @@
+import styled, { css } from "styled-components";
+
+export const Sidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 240px;
+  height: 100vh;
+
+  padding-top: 90px;
+  background-color: #ffffff;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+type SidebarProps = {
+  isSelected: boolean;
+};
+
+export const SidebarContent = styled.div<SidebarProps>`
+  ${({ isSelected }) => css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 12px 0px 12px 24px;
+
+    gap: 0.85rem;
+    color: ${isSelected ? "#0D0D54" : "#4f5b67"};
+    background-color: ${isSelected && "#F7F7FB"};
+    margin-top: 8px;
+    cursor: pointer;
+    text-decoration: none;
+
+    &:hover {
+      background-color: #f7f7fb;
+    }
+  `}
+`;
