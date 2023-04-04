ce8e577db83f4ea2f0d2d1d765982b333df54088
Gustavoctt <gustavocarrertartare@gmail.com>
2023-04-03 21:50:24 -0300

    feat: create router asset

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Asset } from "./pages/Asset";
 import { Home } from "./pages/Home";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/asset" element={<Home />} />
       <Route path="/asset/:id" element={<Asset />} />
+      <Route path="*" element={<Navigate to="/asset" />} />
     </Routes>
   );
 }
